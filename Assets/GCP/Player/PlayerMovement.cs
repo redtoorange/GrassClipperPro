382f92e923244ea7a9280f6323e1b21a74a87301
redtoorange <redtoorange@gmail.com>
2022-05-27 13:56:44 -0400

    revamped player controller

--- Assets/GCP/Player/PlayerMovement.cs
+++ Assets/GCP/Player/PlayerMovement.cs
@@ -4,22 +4,22 @@ namespace GCP.Player
 {
     public class PlayerMovement : MonoBehaviour
     {
+        [Header("Linear")]
         [SerializeField] private float accelerationPower = 5.0f;
-        [SerializeField] private float steeringPower = 5.0f;
+        [SerializeField] private float velocityDeadZone = 0.01f;
 
-        [field: SerializeField]
-        public float Speed { get; private set; }
+        [Header("Turning")]
+        [SerializeField] private float movingTurningPower = 5.0f;
+        [SerializeField] private float staticTurningPower = 10.0f;
 
-        private float steeringAmount;
-        private float speed;
-        private float direction;
 
+        private float linearVelocity = 0.0f;
         private Rigidbody2D rigidbody;
         private Animator animator;
-
         private GameInput gameInput;
-        private Vector2 movementInput;
         private bool hasControl = true;
+        private float turningInput;
+        private float accelerationInput;
 
         private void Start()
         {
@@ -27,42 +27,71 @@ namespace GCP.Player
             gameInput.Enable();
             animator = GetComponent<Animator>();
             rigidbody = GetComponent<Rigidbody2D>();
+
+            rigidbody.centerOfMass = new Vector2(0, 0);
         }
 
         public void EnableControl(bool pHasControl = false) => hasControl = pHasControl;
 
         private void Update()
         {
+            Vector2 movementInput = Vector2.zero;
+
             if (hasControl)
             {
                 movementInput = gameInput.Default.Movement.ReadValue<Vector2>();
             }
-            else
-            {
-                movementInput = Vector2.zero;
-            }
+
+            accelerationInput = movementInput.y;
+            turningInput = -movementInput.x;
         }
 
         private void FixedUpdate()
         {
-            steeringAmount = -movementInput.x;
-            speed = movementInput.y * accelerationPower;
-
+            ApplyRotationInput();
+            ApplyAccelerationInput();
             UpdateAnimation();
+        }
 
-            direction = Mathf.Sign(Vector2.Dot(rigidbody.velocity, rigidbody.GetRelativeVector(Vector2.up)));
+        private void ApplyAccelerationInput()
+        {
+            if (Mathf.Abs(accelerationInput) > 0)
+            {
+                float accelerationAmount = accelerationInput * accelerationPower;
+                rigidbody.AddRelativeForce(new Vector2(0, accelerationAmount * Time.fixedDeltaTime),
+                    ForceMode2D.Impulse);
+            }
 
-            rigidbody.rotation += steeringAmount * steeringPower * rigidbody.velocity.magnitude * direction;
-            rigidbody.AddRelativeForce(Vector2.up * speed);
-            rigidbody.AddRelativeForce(-Vector2.right * rigidbody.velocity.magnitude * steeringAmount / 2.0f);
+            if (rigidbody.velocity.sqrMagnitude < velocityDeadZone)
+            {
+                rigidbody.velocity = Vector2.zero;
+            }
+        }
+
+        private void ApplyRotationInput()
+        {
+            if (Mathf.Abs(turningInput) > 0)
+            {
+                float steeringAmount = 0;
+                if (rigidbody.velocity.sqrMagnitude > 0)
+                {
+                    steeringAmount = turningInput * movingTurningPower;
+                }
+                else
+                {
+                    steeringAmount = turningInput * staticTurningPower;
+                }
+
+                rigidbody.AddTorque(steeringAmount * Time.fixedDeltaTime, ForceMode2D.Impulse);
+            }
         }
 
         private void UpdateAnimation()
         {
             // TODO Move this code to an animation controller script
-            animator.SetFloat("Speed", movementInput.y);
+            animator.SetFloat("Speed", GetSpeed());
 
-            if (Mathf.Abs(speed) > 0)
+            if (Mathf.Abs(GetSpeed()) > 0)
             {
                 animator.SetBool("IsWalking", true);
             }
@@ -72,6 +101,6 @@ namespace GCP.Player
             }
         }
 
-        public float GetSpeed() => speed;
+        public float GetSpeed() => rigidbody.velocity.magnitude;
     }
 }
